4c484b3be3ebe4d5f6fdcd6e01580f5834576b25
khoa.trandang2 <khoa.trandang2@di4l.vn>
2024-09-05 15:13:33 +0700

    update favorite screen

--- lib/screens/favorite_screen/favorite_screen.dart
+++ lib/screens/favorite_screen/favorite_screen.dart
@@ -1,7 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:thichxemphim/boxes.dart';
 import 'package:thichxemphim/models/movie_favorite.dart';
+import 'package:thichxemphim/screens/movie_detail.dart/movie_detail_screen.dart';
+import 'package:thichxemphim/widgets/shimmer.dart';
 
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
@@ -18,19 +22,73 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
         title: Text('Phim đã lưu'),
         centerTitle: true,
       ),
-      body: ValueListenableBuilder(
-        valueListenable: boxFavorites.listenable(),
-        builder: (context, box, _) {
-          return ListView.builder(
-            itemCount: boxFavorites.length,
+      body: _buildBody(),
+    );
+  }
+
+  ValueListenableBuilder<Box<dynamic>> _buildBody() {
+    return ValueListenableBuilder(
+      valueListenable: boxFavorites.listenable(),
+      builder: (context, box, _) {
+        return Padding(
+          padding: EdgeInsets.all(5),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 0.55,
+            ),
+            // shrinkWrap: true,
+            // physics: const NeverScrollableScrollPhysics(),
             itemBuilder: (context, index) {
               final MovieFavorite movie = boxFavorites.getAt(index);
-              return ListTile(
-                title: Text(movie.name ?? ''),
+              return GestureDetector(
+                onTap: () => Get.to(
+                  () => MovieDetailScreen(slug: movie.slug!),
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: _buildCachedNetWorldImage(movie),
+                      ),
+                      Text(
+                        movie.name ?? '',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               );
             },
-          );
-        },
+            itemCount: boxFavorites.length,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildCachedNetWorldImage(MovieFavorite movie) {
+    return CachedNetworkImage(
+      height: 160,
+      imageUrl: 'https://phimimg.com/${movie.poster_url}',
+      fit: BoxFit.cover,
+      placeholder: (context, url) => const ShimmerImage(),
+      errorWidget: (context, url, error) => CachedNetworkImage(
+        height: 160,
+        imageUrl: movie.poster_url ?? '',
+        fit: BoxFit.cover,
+        placeholder: (context, url) => const ShimmerImage(),
+        errorWidget: (context, url, error) => Icon(
+          Icons.error,
+        ),
       ),
     );
   }
--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:thichxemphim/models/movie.dart';
 import 'package:cached_network_image/cached_network_image.dart';
